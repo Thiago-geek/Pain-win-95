a1ea20aa53efeed071de8774ac84d6025e5def65
thiago <thiagoelias365@gmail.com>
2025-02-07 23:06:56 -0300

    agregamos la funcionalidad de dibujar

---
+++ index.js
@@ -0,0 +1,69 @@
+// CONSTS...
+const MODES = {
+    DRAW: 'draw',
+    ERASE: 'erase',
+    RECTANGLE: 'rectangle', 
+    ELLIPSE: 'ellipse',
+    PICKER: 'picker'
+}
+
+// seleccionar elementos del DOM de forma más rápida y sencilla.
+const $ = selector => document.querySelector(selector);
+const $$ = selector => document.querySelectorAll(selector);
+
+
+const $canvas = $('#canvas'); // indica que la variable almacena un elemento del DOM.
+
+const ctx = $canvas.getContext('2d');
+
+// STATE
+let isDrawing = false;
+let startX, startY
+let lastX = 0;
+let lastY = 0;
+let mode = MODES.DRAW;
+
+// EVENTS
+$canvas.addEventListener('mousedown', startDrawing);
+$canvas.addEventListener('mousemove', draw);
+$canvas.addEventListener('mouseup', stopDrawing);
+$canvas.addEventListener('mouseleave', stopDrawing);
+
+
+// METHODS
+function startDrawing(event) {
+    isDrawing = true;
+
+    const {offsetX, offsetY} = event;
+
+    // guardar las conrdenadas iniciales 
+    ;[startX, startY] = [offsetX, offsetY]
+    ;[lastX, lastY] = [offsetX, offsetY]
+
+    console.log({ startX, startY, lastX, lastY});
+}
+
+function draw(event) {
+    if(!isDrawing) return
+
+    const {offsetX,offsetY} = event;
+
+    // comenzar trazado
+    ctx.beginPath();
+
+    // mover el trazado a las cordenadas actuales X e Y
+    ctx.moveTo(lastX, lastY);
+
+    // dibujar una linea entre las cordenadas actuales y las nuevas
+    ctx.lineTo(offsetX,offsetY);
+
+    ctx.stroke();
+
+    // actualizar las ultimas cordenadas
+    ;[lastX,lastY] = [offsetX,offsetY]
+};
+
+function stopDrawing(event) {
+    isDrawing = false; // cuando dejemos de clikear y cuando salgamos del camba para de dibujar
+};
+ 
